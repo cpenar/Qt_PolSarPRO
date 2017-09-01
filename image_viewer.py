64fd0d4760267ae512260d77264e1d14b0155ff0
Carlos Penaranda <carlos.penaranda74@gmail.com>
2017-09-01 18:07:57 +0200

    first

---
+++ image_viewer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- codding: utf-8 -*-
+
+
+
+# -*- coding: utf-8 -*- 
+
+import sys 
+import os 
+
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+
+class MyWindow(QtWidgets.QDialog):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        uic.loadUi('image_viewer.ui', self)
+        self.show()
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWindow()
+    sys.exit(app.exec_())
