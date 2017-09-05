991f58efbac0efff3244c5ffdbf6443135a42e1d
Carlos Penaranda <carlos.penaranda74@gmail.com>
2017-09-05 17:56:34 +0200

    showing image

--- image_viewer.py
+++ image_viewer.py
@@ -9,14 +9,30 @@ import sys
 import os 
 
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtGui import QPixmap
 
-class MyWindow(QtWidgets.QDialog):
+default_image_path = '/home/cpenar/work/PolSARpro/doc_n_data_set/SAN_FRANCISCO_ALOS/T3/PauliRGB.bmp'
+
+class GUI(QtWidgets.QDialog):
     def __init__(self):
-        super(MyWindow, self).__init__()
+        super(GUI, self).__init__()
         uic.loadUi('image_viewer.ui', self)
+
+        self.image = QtGui.QImage(default_image_path)
+        if self.image.isNull():
+            print("ERR: pas d'image chargé")
+
+        self.pixmap = QtGui.QPixmap.fromImage(self.image)
+
+        self.scene = QtWidgets.QGraphicsScene()
+        self.scene.addPixmap(self.pixmap)
+
+        self.imageView.setScene(self.scene)
+
         self.show()
+        self.imageView.fitInView(self.scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    window = MyWindow()
+    window = GUI()
     sys.exit(app.exec_())
