63a4a49248f5f6210a06e0cfbded8579f49a9f63
Carlos Penaranda <carlos.penaranda74@gmail.com>
2017-09-06 14:52:04 +0200

    zooming OK

--- image_viewer.py
+++ image_viewer.py
@@ -7,6 +7,7 @@
 
 import sys 
 import os 
+from logging import warning
 
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtGui import QPixmap
@@ -16,33 +17,63 @@ default_image_path = '/home/cpenar/work/PolSARpro/doc_n_data_set/SAN_FRANCISCO_A
 class GUI(QtWidgets.QDialog):
     def __init__(self):
         super(GUI, self).__init__()
-        # .ui laoder
-        uic.loadUi('image_viewer.ui', self)
+        # Reserved attributes name
+        self.zoomRatio = None
+        self.image = None
+        self.pixmap = None
+        self.scene = None
+        self.ui = None
 
+        # .ui loader
+        self.ui = uic.loadUi('image_viewer.ui')
+
+        # loading image
         self.image = QtGui.QImage(default_image_path)
         if self.image.isNull():
             print("ERR: pas d'image chargé")
 
+        # setting showed pixmap
         self.pixmap = QtGui.QPixmap.fromImage(self.image)
 
         self.scene = QtWidgets.QGraphicsScene()
         self.scene.addPixmap(self.pixmap)
 
-        self.imageView.setScene(self.scene)
+        self.ui.imageView.setScene(self.scene)
 
-        self.show()
+        self.ui.show()
         # must be after show() method
-        self.imageView.fitInView(self.scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
+        self.fitWindow()
 
-        # more stuff
-        self.verticalLayout.resizeEvent = self.onResize
+        # signals connection
+        self.ui.fitImageButton.clicked.connect(self.fitWindow)
+        self.ui.zoomLineEdit.editingFinished.connect(self.setZoomRatio)
 
-    def onResize(self, event):
-        print('toto')
-        print(event)
+    def setZoomRatio(self):
+        try:
+            newZoomRatio = int(self.ui.zoomLineEdit.text())
+        except ValueError:
+            warning("Zoom ratio : only int value allowed")
+            return self.updateZoomRatio()
+
+        if self.zoomRatio != newZoomRatio:
+            self.zoomRatio = newZoomRatio
+            #redraw with new ratio
+            #width = int(self.pixmap.width() * self.zoomRatio / 100)
+            #height = int(self.pixmap.height() * self.zoomRatio / 100)
+            #self.scene.setSceneRect(0, 0, width, height)
+            self.ui.imageView.resetTransform()
+            self.ui.imageView.scale(self.zoomRatio / 100, self.zoomRatio / 100)
+
+    def fitWindow(self):
+        self.ui.imageView.fitInView(self.scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
+        self.updateZoomRatio()
+
+    def updateZoomRatio(self):
+        width_ratio = int(self.ui.imageView.width() / self.pixmap.width() * 100)
+        height_ratio = int(self.ui.imageView.height() / self.pixmap.height() * 100)
+        self.ui.zoomLineEdit.setText(str(min(width_ratio, height_ratio)))
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = GUI()
-    window.resizeEvent = window.onResize
     sys.exit(app.exec_())
