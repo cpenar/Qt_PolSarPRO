9865fe866816d26933271a5637835a79cbd1b698
Carlos Penaranda <carlos.penaranda74@gmail.com>
2017-09-12 17:35:44 +0200

    working on

--- image_viewer.py
+++ image_viewer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 # -*- codding: utf-8 -*-
 
-import sys 
-import os 
-from logging import critical, error, warning, info, debug
+import sys
+#import os
+from logging import error, warning
 
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QGraphicsPixmapItem
 
@@ -14,23 +13,30 @@ from lib.callback_manager import cbManager
 
 default_image_path = '/home/cpenar/work/PolSARpro/doc_n_data_set/SAN_FRANCISCO_ALOS/T3/PauliRGB.bmp'
 
+
 class Window():
     def __init__(self):
         # Reserved attributes name
+        self.ui = None
         self.zoomRatio = None
         self.image = None
         self.pixmap = None
         self.scene = None
-        self.ui = None
         self.currentpoly = []
-        self.polygonCollection = []
         self.cbm = None
+        self.polygonSelection = {
+            'polyQsegments': [],
+            'tempQsegments': [],
+            'currentpoly': [],
+            'polygons': [],
+            }
 
         # .ui loader
         self.ui = uic.loadUi('image_viewer.ui')
 
         # suppress Escape key closing event for Dialog window
-        self.modify_keyPressEvent()
+        self.ui.savedKeyPressEvent = self.ui.keyPressEvent
+        self.ui.keyPressEvent = self.dontCloseWithEscapeKey
 
         # opening and showing the window
         self.ui.show()
@@ -52,6 +58,7 @@ class Window():
         self.scene.addPixmap(self.pixmap)
 
         self.ui.imageView.setScene(self.scene)
+        self.ui.imageView.setMouseTracking(True)
 
         # signals connection
         self.ui.zoomLineEdit.editingFinished.connect(self.setZoomRatio)
@@ -60,64 +67,79 @@ class Window():
         self.ui.actionFit_to_window.triggered.connect(self.fitWindow)
         self.ui.actionDraw_polygon.triggered.connect(self.start_draw_polygon)
 
-        # events connection
-        self.ui.imageView.setMouseTracking(True)
-        self.scene.mouseMoveEvent, self.mouseMoveEventCbm = cbManager(
-                self.scene.mouseMoveEvent, self.mousePosToStatusBar)
+        # events connection using the cbManager
+        self.scene.mouseMoveEvent, self.mouseMoveECbm = cbManager(
+            self.scene.mouseMoveEvent, self.mousePosToStatusBar)
+        self.scene.mousePressEvent, self.mousePressECbm = cbManager(
+            self.scene.mousePressEvent)
 
         # must be after show() method
         self.fitWindow()
+        # a timeout to be sure that we resize the imageView
+        # after the window manager is done decorating the window
+        #QtCore.QTimer.singleShot(500, self.fitWindow)
 
     def start_draw_polygon(self):
-        self.scene.mousePressEvent = self.nextPolyCoord
-        self.mouseMoveEventCbm.connect(self.draw_temp_segment)
+        self.mouseMoveECbm.connect(self.draw_temp_segment)
+        self.mousePressECbm.connect(self.nextPolyCoord)
+        self.polygonSelection['polyQsegments'].append([])
 
     def draw_temp_segment(self, event):
-        debug("Not implemented")
-        pass
-        
+        pSelect = self.polygonSelection
+        x, y = event.lastScenePos().x(), event.lastScenePos().y()
+
+        # Not in image ? -> nothing to do
+        if not (int(x) in range(self.image.width()) and
+                int(y) in range(self.image.height())):
+            return
+
+        # remove previous temp segments
+        while pSelect['tempQsegments']:
+            self.scene.removeItem(pSelect['tempQsegments'].pop())
+
+        if pSelect['currentpoly']:
+            pSelect['tempQsegments'].append(
+                self.draw_segment(pSelect['currentpoly'][-1], (x, y)))
+            pSelect['tempQsegments'].append(
+                self.draw_segment(pSelect['currentpoly'][0], (x, y)))
 
     def nextPolyCoord(self, event):
         # Draw the polygon segments while vertices are
         # defined with mouse clicks.
         # Store the polygon when it is closed.
-        if not event.button() in (Qt.LeftButton, Qt.RightButton): return
+        pSelect = self.polygonSelection
+        if not event.button() in (Qt.LeftButton, Qt.RightButton):
+            return
 
         x, y = event.lastScenePos().x(), event.lastScenePos().y()
 
         # Not in image ? -> nothing to do
-        if not (int(x) in range(self.image.width()) and \
+        if not (int(x) in range(self.image.width()) and
                 int(y) in range(self.image.height())):
             return
 
-        self.currentpoly.append((x, y))
+        pSelect['currentpoly'].append((x, y))
 
         if len(self.currentpoly) > 1:
-            self.draw_segment(self.currentpoly[-1], self.currentpoly[-2])
+            pSelect['polyQsegments'].append(
+                self.draw_segment(self.currentpoly[-1], self.currentpoly[-2]))
 
-        # right button -> last segment
+            # right button -> last segment
         if event.button() == Qt.RightButton:
-            self.polygonCollection.append(self.currentpoly)
+            pSelect['polygons'].append(self.currentpoly)
             # draw last segment
             self.draw_segment(self.currentpoly[-1], self.currentpoly[0])
             # reset currentpoly
             self.currentpoly = []
-            # remove draw_temp_segment callback
-            self.mouseMoveEventCbm.removeCallback(self.draw_temp_segment)
-            # remove mousePressEvent
-            self.scene.mousePressEvent = lambda *args, **kargs: None
+            # remove callbacks
+            self.mouseMoveECbm.removeCallback(self.draw_temp_segment)
+            self.mousePressECbm.removeCallback(self.nextPolyCoord)
 
-        # refresh canvas? view ?
-
-    def draw_segment(self, pt1, pt2, color="k"):
+    def draw_segment(self, pt1, pt2):
         x1, y1, x2, y2 = pt1[0], pt1[1], pt2[0], pt2[1]
         # a styled pen for an easier view not depending of zoom factor
         pen = QtGui.QPen(Qt.black, 0)
         return self.scene.addLine(QtCore.QLineF(x1, y1, x2, y2), pen=pen)
-
-    def modify_keyPressEvent(self):
-        self.ui.savedKeyPressEvent = self.ui.keyPressEvent
-        self.ui.keyPressEvent = self.dontCloseWithEscapeKey
 
     def dontCloseWithEscapeKey(self, event):
         if event.key() != QtCore.Qt.Key_Escape:
@@ -132,7 +154,7 @@ class Window():
             value = self.image.pixel(x, y)
             rgb = str(QtGui.QColor(value).getRgb()[:-1])
             self.ui.statusBar.setText(
-                    'x:' + str(x) + '  y:' + str(y) + '  value:' + rgb)
+                'x:' + str(x) + '  y:' + str(y) + '  value:' + rgb)
 
     def setZoomRatio(self):
         try:
@@ -146,9 +168,9 @@ class Window():
             self.ui.imageView.resetTransform()
             self.ui.imageView.scale(self.zoomRatio / 100, self.zoomRatio / 100)
 
-
     def fitWindow(self):
-        self.ui.imageView.fitInView(self.scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
+        self.ui.imageView.fitInView(
+            self.scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
         self.updateZoomRatio()
 
     def updateZoomRatio(self):
@@ -158,12 +180,11 @@ class Window():
             min(width_ratio, height_ratio), '.2f'))
 
 
-
 class ImageScene(QtWidgets.QGraphicsScene):
     # doesnt do much but need it because overiding
     # Qt builtin method doest work well.
 
-    def __init__(self,parent = None):
+    def __init__(self, parent=None):
         super(ImageScene, self).__init__(parent)
 
     def mouseMoveEvent(self, *args, **kargs):
--- lib/callback_manager.py
+++ lib/callback_manager.py
@@ -1,38 +1,42 @@
 #!/usr/bin/env python3
 # -*- codding: utf-8 -*-
 
-from logging import critical, error, warning, info, debug
+from logging import warning
 
 
-def cbManager(function2overload, firstCallback):
+def cbManager(function2overload, firstCallback=None):
     """
-    Better this function for easy initialisation than
-    the CbCallback class
+    Better use this function for easy initialisation than
+    the CbManager class
 
     Example of use:
 
     Initalisation:
-        function2overload, manager = cbManager(function2overload, firstCallBack)
+        Note: couldn't avoid to write 2 times the 'function2overload'
+        function2overload, manager = cbManager(function2overload)
+        or
+        func2overload, manager = cbManager(func2overload, firstCallBack)
 
-    conencting more callbacks:
+    Connecting more callbacks:
         manager.connect(anotherCallback)
     """
 
     cbm = _CbManager(function2overload)
-    cbm.connect(firstCallback)
     cbm._activatedOriginalFunction = True
+    if firstCallback is not None:
+        cbm.connect(firstCallback)
     return cbm._calls, cbm
 
 
 class _CbManager():
     """
     Qt events doesnt have a connect() method to connect several
-    callback like signals do.
-    This class implement that mechanism.
+    callbacks like signals do.
+    This class implements a similar connect() mechanism.
     """
 
     def __init__(self, function2overload):
-        self.callbacks = []
+        self.callbacks = set()
         self.savedfunc = function2overload
 
     def removeCallback(self, cb):
@@ -43,29 +47,40 @@ class _CbManager():
             warning(cb.__name__ + " doesnt exist in this callback manager.")
             return False
 
-    def removeAllCallback():
-        self.callbacks = []
+    def removeAllCallback(self):
+        self.callbacks.clear()
 
     def connect(self, cb):
         if cb in self.callbacks:
             warning(cb.__name__ + " allready connected. Cant add more")
             return False
         else:
-            self.callbacks.append(cb)
+            self.callbacks.add(cb)
             return True
 
-    def deactivateOriginalFunction():
+    def deactivateOriginalFunction(self):
         self._activatedOriginalFunction = False
 
-    def reactivateOriginalFunction():
+    def reactivateOriginalFunction(self):
         self._activatedOriginalFunction = True
 
-    def resetOriginFunction():
-        self.callbacks = []
-        return self.savedfunc
-        
+    def resetOriginFunction(self):
+        """
+        usage:
+            originalOverloadedFunction = manager.resetOriginFunction()
+        Note:
+            manager wont be usable after this method !
+            so dont do it
+        """
+        # This line disable most of the methods based on the callbacks
+        self.callbacks = None
+
+        return self.originalFunc
+
     def _calls(self, *args, **kargs):
-        for cb in self.callbacks:
+        # copy() to avoid error:
+        # RuntimeError: Set changed size during iteration
+        for cb in self.callbacks.copy():
             cb(*args, **kargs)
         if self._activatedOriginalFunction:
             return self.savedfunc(*args, **kargs)
