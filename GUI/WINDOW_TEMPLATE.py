85400e4ac3a9a54cb9c2d06eb7b6f89f237d37b8
Carlos Penaranda <carlos.penaranda74@gmail.com>
2018-07-24 17:01:12 +0200

    WINDOW_TEMPLATE.py: added **kwargs constructor argument

--- GUI/WINDOW_TEMPLATE.py
+++ GUI/WINDOW_TEMPLATE.py
@@ -5,6 +5,6 @@ from lib.store_window import StoreWindow
 
 
 class Window(StoreWindow):
-    def __init__(self, store):
+    def __init__(self, store, **kwargs):
         super().__init__(__name__, store)
         # stuff to do
